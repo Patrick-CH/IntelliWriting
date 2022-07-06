b8a61cfd49f47f3349d949f4a86974eadb10f4d6
Patrick-CH <1648109733@qq.com>
2022-07-06 21:56:39 +0800

    update sort

--- back_end/get_abstract.py
+++ back_end/get_abstract.py
@@ -10,7 +10,7 @@ def get_abstract(content, num_sentence=6):
     paser = ReTextRankSeq()
     paser.analyze(text=content, lower=True, source='all_filters')
     summary_sentences = paser.get_key_sentences(num=num_sentence, sentence_min_len=6)
-    summary_sentences.sort(key=lambda x: x.index, reverse=True)
+    summary_sentences.sort(key=lambda x: x.index, reverse=False)
     summary = '。'.join([_i.sentence for _i in summary_sentences]) + '。'
     return summary
 
